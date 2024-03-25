a0a391aeb477a57b11eaa9aaef0cc0e484b5ae25
Amandinha2302 <148460065+Amandinha2302@users.noreply.github.com>
2024-03-25 09:24:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const botoes=document.querySelectorAll(".botao");
+const textos=document.querySelectorAll(".botao");
+
 for(let i=0;i<botoes.lenght;i++){
   botoes[i].onclick=function(){
 
     for(let j=0;j<botoes.lenght;j++{
-      botoes.classlist.remove("ativo");
+      botoes[j].classlist.remove("ativo");
+      textos[j].classlist.remove("ativo");
     }
     botoes[i],classlist.add("ativo");
+    textos[i],classlist.add("ativo");
   }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-03-25T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1-tempoAtual;
